8aade2c4e2d1feb68d25c6118f0b07742765ec6a
root <root@vmi1404635.contaboserver.net>
2025-09-28 13:01:59 +0200

    snapshot: snapshot-20250928-130148

--- blocs/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Custom Wagtail StreamField blocks.
-
-This package contains reusable blocks, including a CKEditor-based block.
-"""
-
-
-
--- home/models.py
+++ home/models.py
@@ -1,22 +1,7 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import StreamField
-from wagtail.admin.panels import FieldPanel
-
-from blocs.blocks import CKEditor5Block
 
 
 class HomePage(Page):
-    body = StreamField(
-        [
-            ("contenu", CKEditor5Block()),
-        ],
-        use_json_field=True,
-        blank=True,
-        default=list,
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel("body"),
-    ]
+    pass
